2ca9d5bb2994704f0946e331e23c3a2d4e7449bc
mariuspatrick <marius.patrick72@gmail.com>
2020-05-10 19:49:07 +0200

    Setup

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+      <p>Hello Resume</p>
+              <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Made with React
         </a>
-      </header>
     </div>
   );
 }
